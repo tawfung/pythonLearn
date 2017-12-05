3f609cff67bc36dca7726dfe5edfc1d9811b1171
tawfung <tungvanle@gmail.com>
2017-12-05 17:05:45 +0700

    demo

---
+++ hello.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# -*- coding: utf-42 -*-
+import os, sys
+print ("Hello")
+s="Trai đat nay la cua chung minh"
+print(s)
+import numpy as np
+print ("Numpy:", np.__version__)
+import sympy as sp 
+print ("Sympy:",sp.__version__)
+import sklearn as sk
+print ("scikit-learn:",sk.__version__)
+import pandas as pd 
+print ("pandas:",pd.__version__)
---
+++ tuples.py
@@ -0,0 +1,14 @@
+t=(1,2,3,4)
+print(t[2])
+
+#Set
+
+converted = set([1,2,2,2,3,1,1,3,4,2,3,1,2,4])
+print(converted)
+
+# Loops
+x=[1,2,3,4,5]
+out = []
+for num in x:
+    out.append(num**2)
+print(out)
